b464ee8e946e7f991863d24899e0fa2b90212f87
2024.4.14 <2024.4.14@DESKTOP-NGQKHA6>
2026-06-15 16:01:04 -0400

    Add project files.

---
+++ Models/Role.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace LoginSystem.Models
+{
+    public class Role
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MinLength(1), MaxLength(50)]
+        public string Nome { get; set; }
+
+        public ICollection<UserRole> UserRoles { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace LoginSystem.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MinLength(1), MaxLength(50)]
+        public string Nome { get; set; }
+
+        [Required, MinLength(1), MaxLength(100) ]
+        public string Email { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        //Relacionamento muitos-para-muitos com role
+        public ICollection<UserRole> UserRoles { get; set; }
+
+    }
+}
